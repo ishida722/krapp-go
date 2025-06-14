3dc0174dab0c19d5f4244eadac56a0e517f2f0e3
Tadashi Ishida <ishida@laboro.ai>
2025-06-14 12:20:22 +0000

    note labeler作成中

---
+++ usecase/labeling_note.go
@@ -0,0 +1,16 @@
+package usecase
+
+import (
+	"github.com/ishida722/krapp-go/models"
+)
+
+type NoteLabeler struct {
+	Notes []*models.Note
+	Current *models.Note
+	CurrentIndex int
+}
+
+func (self *NoteLabeler) Next error{
+	self.Current = self.Notes[0]
+	return nil
+}
